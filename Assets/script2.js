528b23e5a68007ef184b8b2f1d18ae96d18f04cc
Andres Malagon <andresmalagon@Andress-MacBook-Pro.local>
2022-04-17 18:00:59 -0400

    fixed password refresh

--- Assets/script2.js
+++ Assets/script2.js
@@ -3,10 +3,11 @@ console.log(lowerCaseLetters)
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 console.log(upperCaseLetters)
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var specialCharacters = ["!", "@", "$", "%", "^", "&", "*"]
+var specialCharacters = ["!", "@", "$", "%", "^", "&", "*", "/", "^"]
 var passwordArray = []
-var password = ""
+
 var generateBtn = document.querySelector("#generate")
+var passwordEl = document.querySelector("#password")
 
 function getRandomPositionInArray(arrayLength) {
     var randomNumber = Math.random();
@@ -15,7 +16,7 @@ function getRandomPositionInArray(arrayLength) {
 
 }
 
-function generateRandomLowerCaseLetter(passLength) {
+function generateRandomLowerCaseLetter(length) {
     var passwordArray = []
 
 
@@ -29,35 +30,43 @@ function generateRandomLowerCaseLetter(passLength) {
 
 }
 
-function generateRandomUpperCaseLetter(passLength) {
-    var passwordArray = []
-    var randomPosition = getRandomPositionInArray(upperCaseLetters.length);
-    var randomLetter = upperCaseLetters[randomPosition]
-    passwordArray.push(randomLetter);
+// function generateRandomUpperCaseLetter(passLength) {
+//     var passwordArray = []
+//     var randomPosition = getRandomPositionInArray(upperCaseLetters.length);
+//     var randomLetter = upperCaseLetters[randomPosition]
+//     passwordArray.push(randomLetter);
 
-    console.log(passwordArray.join(""))
-}
+//     console.log(passwordArray.join(""))
+// }
 
-function generateRandomNumber(passLength) {
-    var passwordArray = []
-    var randomPosition = getRandomPositionInArray(numbers.length);
-    var randomNumber = numbers[randomPosition]
-    passwordArray.push(randomNumber);
+// function generateRandomNumber(passLength) {
+//     var passwordArray = []
+//     var randomPosition = getRandomPositionInArray(numbers.length);
+//     var randomNumber = numbers[randomPosition]
+//     passwordArray.push(randomNumber);
 
-    console.log(passwordArray.join())
-}
+//     console.log(passwordArray.join())
+// }
 
-function generateSpecialChar(passLength) {
-    var passwordArray = []
-    var randomPosition = getRandomPositionInArray(specialCharacters.length);
-    var randomNumber = specialCharacters[randomPosition]
-    passwordArray.push(randomNumber);
+// function generateSpecialChar(passLength) {
+//     var passwordArray = []
+//     var randomPosition = getRandomPositionInArray(specialCharacters.length);
+//     var randomNumber = specialCharacters[randomPosition]
+//     passwordArray.push(randomNumber);
 
-    console.log(passwordArray.join(""))
-}
+//     console.log(passwordArray.join(""))
+// }
 
 function generatePassword() {
+
+    var password = ""
+
+
     var length = window.prompt("Please choose the length of your password. It must be between 8 and 128 characters.")
+    // if (length < 8 || length > 128) {
+    //     window.prompt("Password length must be between 8 and 128 characters")
+    //     generatePassword()
+    // }
     console.log(length)
     var wantLowerCase = window.confirm("Do you want lowercase letters?")
     if (wantLowerCase = true) {
@@ -82,7 +91,7 @@ function generatePassword() {
         password = password + passwordArray[randomCharacterIndex]
     }
     console.log(password)
-    document.querySelector("#password").innerHTML = password
+    passwordEl.innerHTML = password
 
 
 }
